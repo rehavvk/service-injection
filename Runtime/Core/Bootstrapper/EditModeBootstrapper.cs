cdd92e3c8f0a1da40cdf955b2524fef4b739b480
Frederick Böhke <frederick@boehke.com>
2023-02-02 12:45:27 +0100

    Added support for edit mode only bootstrappers to enable use of injection in editor windows etc.

---
+++ Runtime/Core/Bootstrapper/EditModeBootstrapper.cs
@@ -0,0 +1,90 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.Collections.Generic;
+
+namespace Rehawk.ServiceInjection
+{
+    public abstract class EditModeBootstrapper : IBootstrapper
+    {
+        private readonly Queue<object> queuedForResolve = new Queue<object>();
+
+        void IBootstrapper.Boot()
+        {
+            Boot();
+        }
+        
+        void IBootstrapper.Resolve()
+        {
+            while (queuedForResolve.Count > 0)
+            {
+                object instance = queuedForResolve.Dequeue();
+                instance.ResolveDependencies();
+            }
+        }
+
+        void IBootstrapper.BootCompleted()
+        {
+            OnBootCompleted();
+        }
+
+        protected abstract void Boot();
+        protected virtual void OnBootCompleted() {}
+        
+        /// <inheritdoc/>
+        public Registry<T, T> Register<T>()
+        {
+            return ServiceLocator.Register<T>();
+        }
+
+        /// <inheritdoc/>
+        public Registry<TContract, TConcrete> Register<TContract, TConcrete>() where TConcrete : TContract
+        {
+            return ServiceLocator.Register<TContract, TConcrete>();
+        }
+
+        /// <inheritdoc/>
+        public Registry<T, T> RegisterInstance<T>(T instance)
+        {
+            return ServiceLocator.RegisterInstance(instance);
+        }
+
+        /// <inheritdoc/>
+        public Registry<TContract, TConcrete> RegisterInstance<TContract, TConcrete>(TConcrete instance) where TConcrete : TContract
+        {
+            return ServiceLocator.RegisterInstance<TContract, TConcrete>(instance);
+        }
+
+        /// <inheritdoc/>
+        public Registry<T, T> RegisterFactory<T>(Func<T> factory)
+        {
+            return ServiceLocator.RegisterFactory(factory);
+        }
+
+        /// <inheritdoc/>
+        public Registry<TContract, TConcrete> RegisterFactory<TContract, TConcrete>(Func<TConcrete> factory) where TConcrete : TContract
+        {
+            return ServiceLocator.RegisterFactory<TContract, TConcrete>(factory);
+        }
+        
+        /// <inheritdoc/>
+        public T CreateInstance<T>(params object[] args)
+        {
+            return ServiceLocator.CreateInstance<T>(args);
+        }
+
+        /// <inheritdoc/>
+        public object CreateInstance(Type type, params object[] args)
+        {
+            return ServiceLocator.CreateInstance(type, args);
+        }
+        
+        /// <inheritdoc/>
+        public void QueueForResolve<T>(T instance)
+        {
+            queuedForResolve.Enqueue(instance);
+        }
+    }
+}
+
+#endif
---
+++ Runtime/Core/Context/EditModeContext.cs
@@ -0,0 +1,63 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace Rehawk.ServiceInjection
+{
+    [InitializeOnLoad]
+    public static class EditModeContext
+    {
+        private static readonly List<IBootstrapper> bootstrapper = new List<IBootstrapper>();
+
+        static EditModeContext()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+            Run();
+        }
+        
+        public static void AddBootstrapper(IBootstrapper bootstrapper)
+        {
+            EditModeContext.bootstrapper.Add(bootstrapper);
+            Run();
+        }
+        
+        public static void AddBootstrapper<T>() where T : IBootstrapper
+        {
+            AddBootstrapper(Activator.CreateInstance<T>());
+        }
+        
+        public static void Run()
+        {
+            ServiceLocator.Reset();
+            
+            ServiceLocator.BeginRegister();
+            foreach (IBootstrapper bootstrapper in bootstrapper)
+            {
+                bootstrapper.Boot();
+            }
+            ServiceLocator.EndRegister();
+            
+            foreach (IBootstrapper bootstrapper in bootstrapper)
+            {
+                bootstrapper.BootCompleted();
+            }
+
+            foreach (IBootstrapper bootstrapper in bootstrapper)
+            {
+                bootstrapper.Resolve();
+            }
+        }
+        
+        private static void OnPlayModeStateChanged(PlayModeStateChange stateChange)
+        {
+            if (stateChange == PlayModeStateChange.EnteredEditMode)
+            {
+                Run();
+            }
+        }
+    }
+}
+
+#endif
